d3fa09f93932f32a0e81229a00c3142d86c0ef99
jinsu009 <dnqlthsus9@gmail.com>
2026-02-26 08:39:01 +0900

    index page update

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Header } from "./layout/Header";
 
-export const Main = () => {
+const Main = () => {
   return (
     <div>
       <Header />
     </div>
   );
 };
+
+
+export default Main;
